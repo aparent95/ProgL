25dbf02a7d2d811f6ed4ee20403f0d80f61f8d85
Daamyr <maxime.damour974@gmail.com>
2015-11-08 15:54:56 -0500

    correction minime du programme, pas d'ajout or mis des espaces

--- Poker.cpp
+++ Poker.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
 struct T_compo_paquet
 {
-	string sorte ;
-	string valeur ;		//string parce que 10 n'est pas un caractère
-	int valeur_num ; 	//valeur numérique des cartes : 2 pour un 2, 13 pour un as
-	string nom ;
-	bool garder ;
-	bool owned	;  
+	string 	sorte ;
+	string 	valeur ;		//string parce que 10 n'est pas un caractère
+	int 	valeur_num ; 	//valeur numérique des cartes : 2 pour un 2, 13 pour un as
+	string 	nom ;
+	bool 	garder ;
+	bool 	owned	;  
 };
 
 void creation_tas(T_compo_paquet carte[])
 {
-	int count 				=	0 ;
-	string tabsorte[4]		=	{"carreaux", "coeur", "pique", "trefle"} ;
-	string tabvaleur[13]	=	{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "R", "A"}	;		//Je me questionne sur l'utilité de ça, vu qu'on a la table des valeurs numériques.
-	string tabnom[13]		=	{"2", "3", "4", "5", "6", "7", "8", "9", "10", "valet", "dame", "roi", "as"} ;
-	int tabvaleur_num[13]	=	{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14} ;					//Ou alors on fait une fonction qui paramètre ca ? TELLEMENT DE POSSIBILITÉ !!
+	int 	count 				=	0 ;
+	string 	tabsorte[4]			=	{"carreaux", "coeur", "pique", "trefle"} ;
+	string	tabvaleur[13]		=	{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "R", "A"}	;		//Je me questionne sur l'utilité de ça, vu qu'on a la table des valeurs numériques.
+	string 	tabnom[13]			=	{"2", "3", "4", "5", "6", "7", "8", "9", "10", "valet", "dame", "roi", "as"} ;
+	int 	tabvaleur_num[13]	=	{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14} ;					//Ou alors on fait une fonction qui paramètre ca ? TELLEMENT DE POSSIBILITÉ !!
 	
 	while ( count < 51 )
 	{

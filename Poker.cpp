4140b7ee3993caecf3eb2852438f9a4608f87099
Daamyr <maxime.damour974@gmail.com>
2015-11-08 15:38:16 -0500

    Mise à jour de Poker.cpp
    J'ai réussis à créer le jeu de carte, plus aucun problème. Étape 1 dans
    le programme : bouclé
    Par contre, l'algo est faux du coup, alors je m'en vais le modifier.

--- Poker.cpp
+++ Poker.cpp
@@ -4,39 +4,40 @@
 using namespace std;
 
 
-struct T_carte
+struct T_compo_paquet
 {
 	string sorte ;
-	string valeur ;	//string parce que 10 n'est pas un caractère
-	int valeur_num ; //valeur numérique des cartes : 2 pour un 2, 13 pour un as
+	string valeur ;		//string parce que 10 n'est pas un caractère
+	int valeur_num ; 	//valeur numérique des cartes : 2 pour un 2, 13 pour un as
 	string nom ;
 	bool garder ;
 	bool owned	;  
 };
 
-T_carte C_carte(T_carte paquet)
+void creation_tas(T_compo_paquet carte[])
 {
+	int count 				=	0 ;
 	string tabsorte[4]		=	{"carreaux", "coeur", "pique", "trefle"} ;
 	string tabvaleur[13]	=	{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "R", "A"}	;		//Je me questionne sur l'utilité de ça, vu qu'on a la table des valeurs numériques.
 	string tabnom[13]		=	{"2", "3", "4", "5", "6", "7", "8", "9", "10", "valet", "dame", "roi", "as"} ;
 	int tabvaleur_num[13]	=	{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14} ;					//Ou alors on fait une fonction qui paramètre ca ? TELLEMENT DE POSSIBILITÉ !!
 	
-	for ( int i=0; i < 51 ; i++ )
+	while ( count < 51 )
 	{
-		//for ( int j=0 ; j < 3 ; j++ )
-		//{
-			//for ( int k=0 ; k < 12 ; k++ )
-			//{
-				paquet.valeur			= tabvaleur[k] ;
-				//paquet.valeur_num		= tabvaleur_num[k] ;
-				//paquet.nom			= tabnom[k] ;
-				//paquet.garder			= false	;	//quand il choisira quoi garder, s'il n'entre rien, toutes les cartes seront changées
-				//paquet.owned			= false	;	//quand il faudra changer les cartes, ce sera le flag de "est-ce qu'il l'a déjà eu ?"
-													//owned = true quand il aura sa main
-		//	}
-		//}
-	}
-	return paquet ;
+		for ( int i=0 ; i < 4 ; i++ )
+		{
+			for ( int j=0 ; j < 13 ; j++ )
+			{
+				carte[count].sorte			= tabsorte[i] ;
+				carte[count].valeur			= tabvaleur[j] ;
+				carte[count].valeur_num		= tabvaleur_num[j] ;
+				carte[count].nom			= tabnom[j] ;
+				carte[count].garder			= false	;	//quand il choisira quoi garder, s'il n'entre rien, toutes les cartes seront changées
+				carte[count].owned			= false	;	//quand il faudra changer les cartes, ce sera le flag de "est-ce qu'il l'a déjà eu ?"
+				count ++;								//owned = true quand il aura sa main
+			}
+		}
+	}	
 }
 
 
@@ -45,18 +46,16 @@ T_carte C_carte(T_carte paquet)
 */
 
 
-main ()
-{
-	/*string tabsorte[4]		=	{"carreaux", "coeur", "pique", "trefle"} ;
-	string tabvaleur[13]	=	{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "D", "R", "A"}	;		//Je me questionne sur l'utilité de ça, vu qu'on a la table des valeurs numériques.
-	string tabnom[13]		=	{"2", "3", "4", "5", "6", "7", "8", "9", "10", "valet", "dame", "roi", "as"} ;
-	int tabvaleur_num[13]	=	{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13} ;					//Ou alors on fait une fonction qui paramètre ca ? TELLEMENT DE POSSIBILITÉ !!
-	*/
-	T_carte paquet[52] ;
+int main ()
+{	
+	T_compo_paquet carte[52] ;
 
+	creation_tas(carte) ;
 	
-
-	
+//	for ( int i=0 ; i < 52 ; i++ )			//Vérification du tas de carte
+//	{
+//		cout << carte[i].nom << " de " << carte[i].sorte << " (" << carte[i].valeur_num << ") " << i+1 << "e carte" << endl ;
+//	}
 	
 	
 	
